7efb09a990dfb2e5dab65ef8187fe7a34ccf0015
Garrett <garrettodom97@gmail.com>
2021-06-16 12:51:24 -0500

    Adds three seed products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+product = Product.new(name: "Les Paul Guitar", price: 1600, image_url: "", description: "White Les Paul guitar with dual humbuckers and gold hardware")
+product.save
+
+product = Product.new(name: "HP Omen 15", price: 1000, image_url: "", description: "A sick gaming laptop with a 120 Hz screen, a Ryzen 7 processor, and an Nvidia 1660 GTX GPU")
+product.save
+
+prodcut = Product.new(name: "Yeti Tumbler", price: 30, image_url: "", description: "Stainless steel 32 oz tumbler with a lid")
+product.save
