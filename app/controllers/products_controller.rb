50a73a1a3a1be43bb003aaf700878923918e3fec
Garrett <garrettodom97@gmail.com>
2021-06-22 13:54:41 -0500

    Adds stock to serializer and controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -33,6 +33,7 @@ class ProductsController < ApplicationController
     product.price = params[:price] || product.price
     product.image_url = params[:image_url] || product.image_url
     product.description = params[:description] || product.description
+    product.stock = params[:stock] || product.stock
 
     product.save
     render json: product
