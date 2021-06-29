b16f12a7db10ea0af8b32ee32f677bdae35964ee
Garrett <garrettodom97@gmail.com>
2021-06-29 11:47:01 -0500

    Adds order model and associations

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,7 @@ class Product < ApplicationRecord
   validates :stock, numericality: { only_integer: true, greater_than: 0 }
 
   belongs_to :supplier
+  belongs_to :order
 
   def is_discounted?
     if price < 10
